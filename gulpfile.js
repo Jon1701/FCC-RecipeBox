53540cdeaf2cf1a5e9164985e2efe5b875d09aa5
Jon Balon <development@jonbalon.com>
2016-07-10 16:00:51 -0400

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,83 @@
+////////////////////////////////////////////////////////////////////////////////
+// Modules
+////////////////////////////////////////////////////////////////////////////////
+var gulp = require('gulp'); // Gulp.
+var webserver = require('gulp-webserver'); // Gulp webserver.
+var sass = require('gulp-sass'); // SASS.
+var babel = require('gulp-babel'); // JSX to ES6 transpiler.
+
+////////////////////////////////////////////////////////////////////////////////
+// Paths
+////////////////////////////////////////////////////////////////////////////////
+var srcPath = './source/';
+var destPath = './build/';
+var modulesPath = './node_modules/';
+
+////////////////////////////////////////////////////////////////////////////////
+// Tasks
+////////////////////////////////////////////////////////////////////////////////
+
+// JSX.
+gulp.task('jsx', function() {
+  gulp.src(srcPath + 'react-jsx/*')
+    .pipe(babel({ presets: ['es2015', 'react'] }))
+    .pipe(gulp.dest(destPath + 'javascript/'));
+});
+
+// Compile .scss and move.
+gulp.task('stylesheets', function() {
+  gulp.src(srcPath + 'stylesheets/**/*')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(destPath + 'stylesheets/'));
+});
+
+// Move .html.
+gulp.task('html', function() {
+  gulp.src(srcPath + '*.html')
+    .pipe(gulp.dest(destPath));
+});
+
+////////////////////////////////////////////////////////////////////////////////
+// Components.
+////////////////////////////////////////////////////////////////////////////////
+
+// React.
+gulp.task('react', function() {
+
+  // ReactJS
+  gulp.src(modulesPath + 'react/dist/react.js')
+    .pipe(gulp.dest(destPath + 'components/react/'));
+
+  // ReactDOM
+  gulp.src(modulesPath + 'react-dom/dist/react-dom.js')
+    .pipe(gulp.dest(destPath + 'components/react/'));
+
+});
+
+////////////////////////////////////////////////////////////////////////////////
+// Webserver
+////////////////////////////////////////////////////////////////////////////////
+gulp.task("webserver", function() {
+  gulp.src(destPath)
+    .pipe(webserver({
+      fallback: "index.html",
+      livereload: true,
+      directoryListing: false,
+      open: true
+    }));
+});
+
+////////////////////////////////////////////////////////////////////////////////
+// Watch task
+////////////////////////////////////////////////////////////////////////////////
+gulp.task('watch', function() {
+  gulp.watch(srcPath + 'react-jsx/*.jsx', ['jsx']); // JSX files.
+  gulp.watch(srcPath + 'stylesheets/**/*.scss', ['stylesheets']); // SASS Main.
+  gulp.watch(srcPath + 'stylesheets/**/_*.scss', ['stylesheets']); // SASS Partials.
+  gulp.watch(srcPath + '*.html', ['html']);
+});
+
+////////////////////////////////////////////////////////////////////////////////
+// Default task
+////////////////////////////////////////////////////////////////////////////////
+gulp.task('default', ['webserver', 'watch', 'jsx', 'stylesheets', 'html', 'react']);
---
+++ source/react-jsx/index.jsx
@@ -0,0 +1,9 @@
+var RecipeBox = React.createClass({
+  render: function() {
+    return (
+      <div>Hello World</div>
+    )
+  }
+});
+
+ReactDOM.render(<RecipeBox/>, document.getElementById("target"));
