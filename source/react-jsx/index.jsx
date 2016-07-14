6bc4fd2de86b8c342d5567bf3219f152b3fb42db
Jon Balon <development@jonbalon.com>
2016-07-14 18:54:57 -0400

    Added placeholder text and image for RecipeView component, UI change will be needed

--- source/react-jsx/index.jsx
+++ source/react-jsx/index.jsx
@@ -4,19 +4,15 @@ var ViewRecipe = React.createClass({
       <div className="recipe-view">
 
         <div className="title text-center">
-          Architecture
+          {this.props.recipe.name}
         </div>
 
-        <div className="col-2">
+        <div className="ingredients col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          {this.props.recipe.ingredients}
+        </div>
 
-          <div className="ingredients">
-            .ingredients
-          </div>
-
-          <div className="image">
-            .image
-          </div>
-
+        <div className="image col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <img src={this.props.recipe.picture} className="img-responsive"/>
         </div>
 
 
@@ -31,20 +27,25 @@ var Recipe = React.createClass({
   render: function() {
 
     return (
-      <div className="card">
 
-        <div className="card-image">
-          <img className="img-responsive" src={this.props.recipe.picture} />
-          <span className="card-title">{this.props.recipe.name}</span>
+      <div>
+
+        <div className="card">
+
+          <div className="card-image">
+            <img className="img-responsive" src={this.props.recipe.picture} />
+            <span className="card-title">{this.props.recipe.name}</span>
+          </div>
+
+          <div className="card-description">
+            <p>
+              {this.props.recipe.description}
+            </p>
+          </div>
+
         </div>
 
-        <div className="card-description">
-          <p>
-            {this.props.recipe.description}
-          </p>
-        </div>
-
-        <ViewRecipe/>
+        <ViewRecipe recipe={this.props.recipe}/>
 
       </div>
     );
@@ -63,19 +64,19 @@ var RecipeList = React.createClass({
           'name': 'Architecture',
           'picture': 'https://placeholdit.imgix.net/~text?txtsize=80&txt=Recipe&w=1024&h=600',
           'description': 'Sunt id in ea veniam tempor consequat voluptate fugiat elit ad.',
-          ingredients: ['Pumpkin Puree', 'Eggs', 'Pumpkin Pie Spice', 'Pie Crust']
+          'ingredients': 'In in fugiat fugiat dolor commodo dolor sint. Sit ad ullamco ullamco qui in aute consectetur eu veniam est et irure adipisicing mollit occaecat. Exercitation excepteur eiusmod quis elit esse eiusmod laborum do sit fugiat nulla labore amet ut nostrud culpa enim. Qui amet sit ad laboris occaecat consectetur adipisicing. Do sunt esse id tempor aute ut duis proident eiusmod pariatur do sint Lorem. Fugiat Lorem veniam pariatur anim sit commodo exercitation ea ex mollit ut qui excepteur nisi excepteur adipisicing. Dolor sint enim excepteur incididunt id pariatur veniam incididunt. Incididunt irure fugiat in excepteur ut excepteur dolor.'
         },
         {
           'name': 'Spaghetti',
           'picture': 'https://placeholdit.imgix.net/~text?txtsize=80&txt=Recipe&w=1024&h=600',
           'description': 'Aute cupidatat sunt ullamco et laborum esse reprehenderit tempor.',
-          ingredients: ['Noodles', 'Tomato Sauce', 'Meatballs']
+          'ingredients': 'Veniam ea est amet sit eu reprehenderit magna ipsum eu. Nulla fugiat consectetur Lorem nostrud adipisicing pariatur aliqua quis quis sit aute. Aliquip labore excepteur velit quis exercitation id nisi fugiat proident duis ex aliquip.'
         },
         {
           'name': 'Onion Pie',
           'picture': 'https://placeholdit.imgix.net/~text?txtsize=80&txt=Recipe&w=1024&h=600',
           'description': 'Proident culpa eiusmod ullamco adipisicing eu anim nulla tempor do officia.',
-          ingredients: ['Onion', 'Pie Crust']
+          'ingredients': 'Elit eu pariatur amet ut sit dolor veniam ut enim nostrud anim occaecat proident aute aliquip occaecat. Eiusmod sit dolore Lorem consequat cupidatat officia amet ea ex tempor sunt veniam. Enim dolore eiusmod dolore quis Lorem duis qui amet ipsum. Adipisicing occaecat occaecat ut eiusmod dolore elit dolor mollit incididunt. Pariatur mollit reprehenderit exercitation proident est voluptate elit sint culpa cupidatat et. Est minim amet duis ea nostrud voluptate veniam ex occaecat occaecat ex cupidatat commodo dolor. Eu esse et id esse voluptate cupidatat consectetur est voluptate et sunt eiusmod cupidatat et id et.'
         }
       ]
     }
