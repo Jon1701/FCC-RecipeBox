2f7adfbd2927dc39044ab4b5c099195d433f88e0
Jon Balon <development@jonbalon.com>
2016-07-15 03:07:45 -0400

    used different placeholder images

--- source/react-jsx/index.jsx
+++ source/react-jsx/index.jsx
@@ -55,19 +55,19 @@ var RecipeList = React.createClass({
       recipes: [
         {
           'name': 'Architecture',
-          'picture': '../media/images/placeholders/recipe-cover.png',
+          'picture': '../media/images/placeholders/architecture.jpg',
           'description': 'Sunt id in ea veniam tempor consequat voluptate fugiat elit ad.',
           'ingredients': 'In in fugiat fugiat dolor commodo dolor sint. Sit ad ullamco ullamco qui in aute consectetur eu veniam est et irure adipisicing mollit occaecat. Exercitation excepteur eiusmod quis elit esse eiusmod laborum do sit fugiat nulla labore amet ut nostrud culpa enim. Qui amet sit ad laboris occaecat consectetur adipisicing. Do sunt esse id tempor aute ut duis proident eiusmod pariatur do sint Lorem. Fugiat Lorem veniam pariatur anim sit commodo exercitation ea ex mollit ut qui excepteur nisi excepteur adipisicing. Dolor sint enim excepteur incididunt id pariatur veniam incididunt. Incididunt irure fugiat in excepteur ut excepteur dolor.'
         },
         {
           'name': 'Spaghetti',
-          'picture': '../media/images/placeholders/recipe-cover.png',
+          'picture': '../media/images/placeholders/spaghetti.jpg',
           'description': 'Aute cupidatat sunt ullamco et laborum esse reprehenderit tempor.',
           'ingredients': 'Veniam ea est amet sit eu reprehenderit magna ipsum eu. Nulla fugiat consectetur Lorem nostrud adipisicing pariatur aliqua quis quis sit aute. Aliquip labore excepteur velit quis exercitation id nisi fugiat proident duis ex aliquip.'
         },
         {
-          'name': 'Onion Pie',
-          'picture': '../media/images/placeholders/recipe-cover.png',
+          'name': 'Cake',
+          'picture': '../media/images/placeholders/cake.jpg',
           'description': 'Proident culpa eiusmod ullamco adipisicing eu anim nulla tempor do officia.',
           'ingredients': 'Elit eu pariatur amet ut sit dolor veniam ut enim nostrud anim occaecat proident aute aliquip occaecat. Eiusmod sit dolore Lorem consequat cupidatat officia amet ea ex tempor sunt veniam. Enim dolore eiusmod dolore quis Lorem duis qui amet ipsum. Adipisicing occaecat occaecat ut eiusmod dolore elit dolor mollit incididunt. Pariatur mollit reprehenderit exercitation proident est voluptate elit sint culpa cupidatat et. Est minim amet duis ea nostrud voluptate veniam ex occaecat occaecat ex cupidatat commodo dolor. Eu esse et id esse voluptate cupidatat consectetur est voluptate et sunt eiusmod cupidatat et id et.'
         }
