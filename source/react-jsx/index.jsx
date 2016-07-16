c6a5a71bc880dd517970157cbe4fe7c62a70f488
Jon Balon <development@jonbalon.com>
2016-07-16 01:37:50 -0400

    Add ids to recipe data, refactor code

--- source/react-jsx/index.jsx
+++ source/react-jsx/index.jsx
@@ -83,23 +83,30 @@ var Recipe = React.createClass({
 // Component which contains all recipe data.
 var RecipeList = React.createClass({
 
+  updateRecipe: function(id) {
+
+  },
+
   // Default recipes.
   getInitialState: function() {
     return {
       recipes: [
         {
+          'id': 0,
           'name': 'Architecture',
           'picture': '../media/images/placeholders/architecture.jpg',
           'description': 'Sunt id in ea veniam tempor consequat voluptate fugiat elit ad.',
           'ingredients': 'Mollit pariatur aute voluptate pariatur amet consectetur eu adipisicing fugiat aliquip nulla officia enim.'
         },
         {
+          'id': 1,
           'name': 'Spaghetti',
           'picture': '../media/images/placeholders/spaghetti.jpg',
           'description': 'Aute cupidatat sunt ullamco et laborum esse reprehenderit tempor.',
           'ingredients': 'Tempor cillum anim occaecat et enim proident reprehenderit commodo dolore ex irure deserunt tempor Lorem labore.'
         },
         {
+          'id': 2,
           'name': 'Cake',
           'picture': '../media/images/placeholders/cake.jpg',
           'description': 'Proident culpa eiusmod ullamco adipisicing eu anim nulla tempor do officia.',
@@ -111,13 +118,13 @@ var RecipeList = React.createClass({
 
   render: function() {
 
-    var temp = this.state.recipes.map(function(element, index) {
-      return <Recipe recipe={element} key={index}/>
-    })
+    var objThis = this;
 
     return (
       <div>
-        {temp}
+        {this.state.recipes.map(function(element, index) {
+          return <Recipe recipe={element} updater={objThis.updateRecipe} key={index}/>
+        })}
       </div>
     );
   }
