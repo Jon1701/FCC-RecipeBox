d9c0b63579f3f3ab38bb1215ec44e95d27d57976
Jon Balon <development@jonbalon.com>
2016-07-28 18:05:19 -0400

    Moved jquery and materialize js into index.ksx

--- source/react-jsx/index.jsx
+++ source/react-jsx/index.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import RecipeList from './RecipeList.jsx';
 
+// Recipe App
+import RecipeList from './RecipeList.jsx';
 ReactDOM.render(<RecipeList/>, document.getElementById("target"));
+
+// jQuery
+import $ from 'jquery';
+
+// Materialize
+import 'materialize-css';
