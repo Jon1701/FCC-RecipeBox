e3d059b805b2a7a79853da474a273b9a1fab572c
Jon Balon <development@jonbalon.com>
2016-07-28 17:57:05 -0400

    Removed <RecipeContainer/> and made <RecipeList/> as the top-level container

--- source/react-jsx/index.jsx
+++ source/react-jsx/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import RecipeContainer from './RecipeContainer.jsx';
+import RecipeList from './RecipeList.jsx';
 
-ReactDOM.render(<RecipeContainer/>, document.getElementById("target"));
+ReactDOM.render(<RecipeList/>, document.getElementById("target"));
